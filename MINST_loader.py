20c75371efb85417a5c46defebf36516c1f4f974
alr11118 <alara.engin2008@gmail.com>
2026-09-18 19:19:17 -0700

    comment out testing pront statements

--- MINST_loader.py
+++ MINST_loader.py
@@ -2,30 +2,30 @@ def loadImages(filename):
     with open(filename, "rb") as file:
         data = file.read()
 
-    print(data[0:16])
     magic = int.from_bytes(data[0:4], byteorder="big")
     numImages = int.from_bytes(data[4:8], byteorder="big")
     numRows = int.from_bytes(data[8:12], byteorder="big")
     numColumns = int.from_bytes(data[12:16], byteorder="big")
 
+    """
     print(magic)
     print(numImages)
     print(numRows)
     print(numColumns)
+    """
 
-    firstImage = data[16: 16 + 784]
+    images = []
+    for imageIndex in range(numImages):
+        image = []
+        for pixelIndex in range(numRows * numColumns):
+            index = 16 + imageIndex * (numRows * numColumns) + pixelIndex
+            value = (data[index] / 255)
+            image.append(value)
+        images.append(image)
+    return images
 
-    print(len(firstImage))
-    normalisedImage = []
-    for row in range(28):
-        for col in range(28):
-            index = 28 * row + col
-            pixelValue = (firstImage[index] / 255)
-            normalisedImage.append(pixelValue)
-            #print(pixelValue, end = " ")
-        #print("")
-
-images = loadImages("MINST/train-images.idx3-ubyte")
+# Example Usage:
+#images = loadImages("MINST/train-images.idx3-ubyte")
 
 def loadLabels(filename):
     with open(filename, "rb") as file:
@@ -34,11 +34,17 @@ def loadLabels(filename):
     magic = int.from_bytes(data[0:4], byteorder="big")
     numLabels = int.from_bytes(data[4:8], byteorder="big")
 
+    """
     print("Magic:", magic)
     print("Number of labels:", numLabels)
+    """
 
-    firstLabel = data[8]
+    labels = []
+    for labelIndex in range(numLabels):
+        index = 8 + labelIndex
+        value = data[index]
+        labels.append(value)
+    return labels
 
-    print("First label:", firstLabel)
-
-labels = loadLabels("MINST/train-labels.idx1-ubyte")
+# Example Usage:
+#labels = loadLabels("MINST/train-labels.idx1-ubyte")
