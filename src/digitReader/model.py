dd6cb03d89705de994da03e98a0787b7afc2759d
alr11118 <alara.engin2008@gmail.com>
2026-09-22 21:01:22 -0700

    fixed weight file path bug

--- src/digitReader/model.py
+++ src/digitReader/model.py
@@ -3,7 +3,7 @@ from . import weightLoader
 
 def predict(input):
     
-    (hiddenWeights, hiddenBiases, outputWeights,outputBiases) = weightLoader.loadWeights("weights.json")
+    (hiddenWeights, hiddenBiases, outputWeights,outputBiases) = weightLoader.loadWeights()
     probabilities, _, _ = train.forwardPass(
         input,
         hiddenWeights,
--- src/digitReader/weightLoader.py
+++ src/digitReader/weightLoader.py
@@ -1,11 +1,15 @@
 import json
-def loadWeights(filePath):
-    with open(filePath, "r") as file:
+from importlib.resources import files
+
+def loadWeights():
+    weightsPath = files("digitReader").joinpath("weights.json")
+
+    with weightsPath.open("r", encoding="utf-8") as file:
         data = json.load(file)
-    
+
     hiddenWeights = data["hiddenWeights"]
     hiddenBiases = data["hiddenBiases"]
     outputWeights = data["outputWeights"]
     outputBiases = data["outputBiases"]
-    
+
     return hiddenWeights, hiddenBiases, outputWeights, outputBiases
